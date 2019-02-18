2b969f3569e0f2b0f45248023bcbef2e33a92e68
DaichiSaito <h1a_ble@yahoo.co.jp>
2019-02-18 14:45:21 +0900

    GoogleMapページをトップページに持ってきた

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import GMap from './views/GMap.vue'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
@@ -10,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'gmap',
+      component: GMap
     },
     {
       path: '/about',
---
+++ src/views/GMap.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="map">
+      <div class="google-map" id="map">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'gmap',
+  data() {
+    return {
+        lat: 35.689,
+        lng: 139.691
+    }
+  },
+  methods: {
+    renderMap() {
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: this.lat, lng: this.lng },
+        zoom: 1,
+        maxZoom: 15,
+        minZoom: 3,
+        streetViewControl: false,
+        mapTypeControl: false,
+      })
+    }
+  },
+
+  mounted() {
+    this.renderMap()
+  }
+}
+</script>
+
+<style>
+.google-map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
