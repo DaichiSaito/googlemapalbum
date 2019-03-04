b3d4c998dc4f223efaf6597640ba3af89300b800
DaichiSaito <h1a_ble@yahoo.co.jp>
2019-03-04 14:44:52 +0900

    ログイン時のプログレス表示

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,41 @@
 <template lang="html">
   <div class="signup container">
-    <v-btn @click="signIn" color="error">Googleログイン</v-btn>
+    <Loading v-if="progress" />
+    <v-btn @click="signIn" :disabled="progress" color="error">Googleログイン</v-btn>
   </div>
 </template>
 
 <script>
 import firebase from "firebase";
 import { auth } from "@/firebase/init";
+import Loading from "@/components/Loading.vue";
 
 export default {
   name: "login",
 
+  components: {
+    Loading
+  },
+
+  data() {
+    return {
+      progress: false
+    };
+  },
+
   methods: {
     signIn() {
+      this.progress = true;
       const provider = new firebase.auth.GoogleAuthProvider();
       firebase
         .auth()
         .signInWithPopup(provider)
         .then(result => {
+          this.progress = false;
           if (result.user) {
             this.$router.push("/");
+          } else {
+            alert("ログイン失敗");
           }
         });
     }
